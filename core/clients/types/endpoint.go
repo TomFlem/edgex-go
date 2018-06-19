7e67336dd0f7b5ef7122a121e1aedca09a3dd0a7
Tom Fleming <tom@iotechsys.com>
2018-06-19 10:24:37 +0100

    EG-19 fixed busy wait in endpoint.go
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- core/clients/types/endpoint.go
+++ core/clients/types/endpoint.go
@@ -10,16 +10,13 @@ import (
 type Endpoint struct {}
 
 func(e Endpoint) Monitor(params EndpointParams, ch chan string) {
-	check := time.Now()
 	for true {
-		if time.Now().After(check) {
-			data, err := consulclient.GetServiceEndpoint(params.ServiceKey)
-			if err != nil {
-				fmt.Fprintln(os.Stdout, err.Error())
-			}
-			url := fmt.Sprintf("http://%s:%v%s", data.Address, data.Port, params.Path)
-			ch <- url
-			check = time.Now().Add(time.Second * time.Duration(15))
+		data, err := consulclient.GetServiceEndpoint(params.ServiceKey)
+		if err != nil {
+			fmt.Fprintln(os.Stdout, err.Error())
 		}
+		url := fmt.Sprintf("http://%s:%v%s", data.Address, data.Port, params.Path)
+		ch <- url
+		time.Sleep(15000 * time.Millisecond)
 	}
 }
